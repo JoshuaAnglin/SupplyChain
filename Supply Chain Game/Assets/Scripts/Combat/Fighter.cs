8eb385ae58b42a37b874d39fbcc8f4d4fd436e7d
DavidMorgan00 <dm1532x@gre.ac.uk>
2021-09-30 17:37:24 +0100

    Changed version

--- Supply Chain Game/Assets/Scripts/Combat/Fighter.cs
+++ Supply Chain Game/Assets/Scripts/Combat/Fighter.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SCG.Fighter
+namespace SCG.Combat
 {
     public class Fighter : MonoBehaviour
     {
+        
         public void Attack()
         {
             print("Take that!");
